ef28bec3b06437231d6dd27435ce4640698ad6bb
Saul Muñoz <sam.pedreros14@gmail.com>
2023-08-09 18:14:59 -0400

    Up

--- Learn Java/hotel.java
+++ Learn Java/hotel.java
@@ -4,5 +4,13 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // start coding here
+        String name1 = scanner.next();
+        String line1 = scanner.nextLine();
+        String name2 = scanner.next();
+        String line2 = scanner.nextLine();
+        String name3 = scanner.next();
+        System.out.println(name1);
+        System.out.println(name2);
+        System.out.println(name3);
     }
 }
