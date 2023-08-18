1a05f49488bf5e1a5369cf605daa7be1b0117ff6
Saul Muñoz <sam.pedreros14@gmail.com>
2023-08-18 16:15:46 -0400

    New Excersice

--- Learn Java/Ejercicios/Chocolate.java
+++ Learn Java/Ejercicios/Chocolate.java
@@ -2,18 +2,11 @@ import java.util.Scanner;
 
 class Chocolate {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // start coding here
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        int k = scanner.nextInt();
-
-        if ((n * m) < k) {
-            System.out.println("NO");
-        } else if (k % m == 0 || k % n == 0) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(i);
+                break;
+            }
         }
     }
 }
