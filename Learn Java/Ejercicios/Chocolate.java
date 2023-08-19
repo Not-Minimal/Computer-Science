72cae087a47fd49d81ecf7d6928c6994513d3b33
Saul Muñoz <sam.pedreros14@gmail.com>
2023-08-19 15:57:56 -0400

    Up

--- Learn Java/Ejercicios/Chocolate.java
+++ Learn Java/Ejercicios/Chocolate.java
@@ -2,11 +2,6 @@ import java.util.Scanner;
 
 class Chocolate {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(i);
-                break;
-            }
-        }
+        System.out.println(2 + 4 * 5 - 10 / (2 + 1));
     }
 }
